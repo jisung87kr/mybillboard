ff8e7b345a1c58b94772c96f9b0b446a9977d2ff
jiseong <jisung87kr@nate.com>
2021-01-31 00:06:42 +0900

    유투브 data api 사용 임시로 막기

--- app/Http/Controllers/YoutubeDataApiController.php
+++ app/Http/Controllers/YoutubeDataApiController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 //use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Blade;
-
 class YoutubeDataApiController extends Controller
 {
     public $clientSecret;
@@ -22,27 +21,35 @@ class YoutubeDataApiController extends Controller
 
     public function index()
     {
-        $searchResponse = null;
+        $searchResponse = json_decode(Storage::get('public/ytresponse.json'));
         return view('youtube.index', compact('searchResponse'));
     }
 
     public function search(Request $request)
     {
-        $searchResponse = null;
-        $this->searchInit();
-
-        $searchResponse = $this->youtube->search->listSearch('id,snippet', array(
-            'q' => $request->q,
-            'order' => $this->order,
-            'maxResults' => $this->maxResults,
-        ));
-
-        $searchResponse = $searchResponse->items;
+        $searchResponse = json_decode(Storage::get('public/ytresponse.json'));
         if($request->ajax()){
             $component = view('components.youtube-item-list')->with(compact('searchResponse'))->render();
             return response()->json($component);
         }
+
         return view('youtube.index', compact('searchResponse'));
+
+//        $searchResponse = null;
+//        $this->searchInit();
+//
+//        $searchResponse = $this->youtube->search->listSearch('id,snippet', array(
+//            'q' => $request->q,
+//            'order' => $this->order,
+//            'maxResults' => $this->maxResults,
+//        ));
+//
+//        $searchResponse = $searchResponse->items;
+//        if($request->ajax()){
+//            $component = view('components.youtube-item-list')->with(compact('searchResponse'))->render();
+//            return response()->json($component);
+//        }
+//        return view('youtube.index', compact('searchResponse'));
     }
 
     public function searchInit()
@@ -50,5 +57,11 @@ class YoutubeDataApiController extends Controller
         $this->client = new \Google_Client();
         $this->client->setDeveloperKey($this->clientSecret['apiKey']);
         $this->youtube = new \Google_Service_YouTube($this->client);
+    }
+
+    public function storePlaylist(Request $request)
+    {
+        ddd($request->all());
+        return false;
     }
 }
